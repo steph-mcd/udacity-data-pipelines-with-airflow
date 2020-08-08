0a0f9c1f369a3a7e98dd2cec59c13f4e33cb5a43
stephaniemcdonald <56379358+stephaniemcdonald@users.noreply.github.com>
2020-08-08 17:48:37 +1000

    SQL queries, fact & dim tables
    - Updated SQL queries to include insert
    - Added Fact and Dimension table loader operators

--- project_template 3/plugins/operators/load_dimension.py
+++ project_template 3/plugins/operators/load_dimension.py
@@ -1,6 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from helpers import SqlQueries
 
 class LoadDimensionOperator(BaseOperator):
 
@@ -8,15 +9,26 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id = "",
+                 table = "",
+                 sql = "",
+                 append_only = False,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql = sql
+        self.append_only = append_only
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        if not self.append_only:
+            self.log.info("Deleting from {} table".format(self.table))
+            redshift.run("DELETE FROM {}".format(self.table)) 
+
+        self.log.info("Inserting into {} table".format(self.table))
+        formatted_sql = getattr(SqlQueries,self.sql).format(self.table)
+        redshift.run(formatted_sql)
--- project_template 3/plugins/operators/load_fact.py
+++ project_template 3/plugins/operators/load_fact.py
@@ -1,22 +1,34 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from helpers import SqlQueries
 
 class LoadFactOperator(BaseOperator):
-
+    
     ui_color = '#F98866'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id = "",
+                 table = "",
+                 sql = "",        
+                 append_only = False,
                  *args, **kwargs):
-
+                    
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql = sql
+        self.append_only = append_only
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if not self.append_only:
+            self.log.info("Deleting from {} table".format(self.table))
+            redshift.run("DELETE FROM {}".format(self.table))         
+        self.log.info("Inserting into {} table".format(self.table))
+        
+        formatted_sql = getattr(SqlQueries,self.sql).format(self.table)
+        redshift.run(formatted_sql)
