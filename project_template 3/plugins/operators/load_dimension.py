26088aafc4d9bfbf86a9d532fb32e95dc138c82c
stephaniemcdonald <56379358+stephaniemcdonald@users.noreply.github.com>
2020-08-13 19:30:17 +1000

    feat: added append only option for dim table

--- project_template 3/plugins/operators/load_dimension.py
+++ project_template 3/plugins/operators/load_dimension.py
@@ -4,7 +4,15 @@ from airflow.utils.decorators import apply_defaults
 from helpers import SqlQueries
 
 class LoadDimensionOperator(BaseOperator):
-
+    """
+    Reads data from staging tables in redshift and inserts into songplays table
+    
+    :param redshift_conn_id: Redshift connection id
+    :param table: name of dimension table
+    :param sql: name of insert sql query from SqlQueries
+    :param primary_key: this is for when we are using append only - this is teh column used to check if the row already exists
+    """
+    
     ui_color = '#80BD9E'
 
     @apply_defaults
@@ -12,7 +20,8 @@ class LoadDimensionOperator(BaseOperator):
                  redshift_conn_id = "",
                  table = "",
                  sql = "",
-                 append_only = False,
+                 append_only = "",
+                 primary_key = "",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
@@ -21,14 +30,29 @@ class LoadDimensionOperator(BaseOperator):
         self.table = table
         self.sql = sql
         self.append_only = append_only
+        self.primary_key = primary_key
 
     def execute(self, context):
-        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         
-        if not self.append_only:
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        if self.append_only:
+            if self.append_insert:
+            table_insert_sql = f"""
+                create temp table stage_{self.table} (like {self.table}); 
+                
+                insert into stage_{self.table}
+                {self.select_sql};
+                
+                delete from {self.table}
+                using stage_{self.table}
+                where {self.table}.{self.primary_key} = stage_{self.table}.{self.primary_key};
+                
+                insert into {self.table}
+                select * from stage_{self.table};
+            """
+        else:
             self.log.info("Deleting from {} table".format(self.table))
-            redshift.run("DELETE FROM {}".format(self.table)) 
-
+            redshift.run("DELETE FROM {}".format(self.table))        
         self.log.info("Inserting into {} table".format(self.table))
         formatted_sql = getattr(SqlQueries,self.sql).format(self.table)
         redshift.run(formatted_sql)
